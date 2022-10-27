80600efd3030542cad46190f718a7a36ef022e91
Aloysius Ng <aloysius.ng2000@hotmail.com>
2022-10-27 19:31:22 +0800

    tt

--- MOBLIMA/src/entities/MovieResolution.java
+++ MOBLIMA/src/entities/MovieResolution.java
@@ -1,1 +1,34 @@
 package entities;
+public enum MovieResolution {
+	TWOD ("TWOD"),
+    THREED ("THREED"),
+    IMAX ("IMAX");
+
+    private final String name;
+
+    /**
+     * Constructor for the MovieFormat enum, taking in the string value of the enum and setting it as an attribute.
+     * @param s of the enum.
+     */
+    private MovieResolution(String s) {
+        name = s;
+    }
+
+    /**
+     * For string comparison.
+     * @param otherName String to be compared to.
+     * @return boolean on whether the String value of MovieFormat is equals to otherName.
+     */
+    public boolean equalsString(String otherName) {
+        // (otherName == null) check is not needed because name.equals(null) returns false 
+        return name.equals(otherName);
+    }
+
+    /**
+     *
+     * @return String value of MovieFormat for string comparison purposes.
+     */
+    public String toString() {
+       return this.name;
+    }
+}
