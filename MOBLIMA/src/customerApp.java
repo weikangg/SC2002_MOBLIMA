0661e39748c43c213313b500b64cfebb99bc2ebb
LingHin <66077386+LingHin@users.noreply.github.com>
2022-11-01 12:37:16 +0800

    Update customerApp.java

--- MOBLIMA/src/customerApp.java
+++ MOBLIMA/src/customerApp.java
@@ -21,6 +21,7 @@ public class customerApp {
             //main menu
             System.out.print("================= MOBLIMA CUSTOMER INTERFACE =================\n"+
             "1. Log In"+
+            "2. Create account"+
             "2. Search\n"+
             "3. Top 5 Movies\n"+
             "0. Back to main menu\n"+
@@ -40,9 +41,13 @@ public class customerApp {
 
                 case 2:
                 counter = 0;
+                //ask user for information to create account
+
+                case 3:
+                counter = 0;
                 //search for movie
                 
-                case 3:
+                case 4:
                 counter = 0;
                 //show top 5 movies
                 
@@ -86,6 +91,8 @@ public class customerApp {
             switch(input){
                 case 1:
                 //book ticket
+                    
+                //after showing user the seats for a movie, call bookingmenu in booking manager
 
                 case 2:
                 //show booking history
@@ -106,4 +113,21 @@ public class customerApp {
             }
         }while(exit == false);
     }
+    
+    /*public void findSeats(){
+        String movieName;
+        int cineplex;
+
+        System.out.println("Please choose a Cineplex:")
+        cineplex = scan.nextInt();
+
+        
+
+        System.out.println("Please enter movie name:");
+        movieName = scan.nextLine();
+
+        //ask user for movie and find movie seating file
+        //open csv file
+        //go through the csv file, for if seat is occupied print [X], if seat is not occupied print [0]
+    }*/
 }
