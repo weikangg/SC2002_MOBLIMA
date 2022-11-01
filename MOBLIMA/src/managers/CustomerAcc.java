9e3d4b326089f5b0fc8b6e203e462d0068a9119e
LingHin <66077386+LingHin@users.noreply.github.com>
2022-11-01 12:30:06 +0800

    Update CustomerAcc.java

--- MOBLIMA/src/managers/CustomerAcc.java
+++ MOBLIMA/src/managers/CustomerAcc.java
@@ -1,5 +1,6 @@
 package managers;
 
+import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.ArrayList;
 import entities.*;
@@ -21,6 +22,41 @@ public class CustomerAcc {
         this.age = age;
     }
 
+/*    public ArrayList<CustomerAcc> readCustomerFile(){
+        //read in account.csv file, then put the names, email, mobile and age into constructor and create an array of customer information
+
+    }
+
+    public CustomerAcc createAcc(){
+        String name;
+        String email;
+        int mobile;
+        int age;
+
+        //ask for user information and use try to check if name or email or mobile exists
+        
+        
+        
+        //to add exceptions
+        System.out.println("Enter username: ");
+        name = scan.nextLine();
+         
+        System.out.println("Enter your email:");
+        email = scan.nextLine();
+
+        System.out.println("Enter mobile:");
+        mobile = scan.nextInt();
+
+        System.out.println("Enter age:");
+        age = scan.nextInt();
+
+        CustomerAcc acc = new CustomerAcc(name, email, mobile, age);
+
+        //add into csv file
+        //for each account created, create a file inside tranaction
+    }
+*/
+    /*User's history will be update as the user uses the app. History will not be saved into csv file. If app shuts down, history will be gone*/
     public void updateHistory(ArrayList<Ticket> tix, String movieName, String tranDateTime, String cinID){
         //creates transaction id
         String id =  cinID + tranDateTime;
