4fef2e90c3926278201dc35048b9919275242092
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-03 00:00:48 +0800

    Update TicketPrice.java

--- MOBLIMA/src/entities/TicketPrice.java
+++ MOBLIMA/src/entities/TicketPrice.java
@@ -1,32 +1,77 @@
 package entities;
-import entities.Types.TicketType;
 
-public class TicketPrice {
+import java.io.Serializable;
+import java.util.EnumMap;
+
+public class TicketPrice implements Serializable{
+	private EnumMap<TicketType, Double> priceList = new EnumMap<>(TicketType.class);
+	private EnumMap<MovieType, Double> mtList = new EnumMap<>(MovieType.class);
 	private Double[] price;
+	private Double[] mtPrice;
 	
-	public TicketPrice(Double[] price)
+
+
+	/*
+	 * constructer
+	 */
+	public TicketPrice()
 	{
-		this.price = new Double[]{10.00,14.50,5.0,7.0};
+		this.price = new Double[]{8.50,9.50,9.50,11.0,11.0,4.0,7.0};
+		this.mtPrice = new Double[]{1.00,1.29,1.45,1.50};
+		int x = 0;
+		for(TicketType type: TicketType.values())
+		{
+			priceList.put(type,price[x]);
+			x++;
+		}
+		x=0;
+		for(MovieType mt: MovieType.values())
+		{
+			mtList.put(mt,mtPrice[x]);
+			x++;
+		}
 	}
-	
-	public Double getPrice(int price)
+
+
+	/*
+	 * getters
+	 */
+	public Double getPrice(int index)
 	{
-		return this.price[price];
+		return this.price[index];
 	}
-	
-	/*public void hasPromotion()
+	public Double getMtPrice(int index)
 	{
-		this.price
-	}*/
-	
-	
-	public Double[] getPriceList()
+		return this.mtPrice[index];
+	}
+	public Double[] getPrices()
 	{
 		return this.price;
 	}
-	
-	public void setPrice(Double price, int ticket)
+	public EnumMap<TicketType, Double> getMappedPrice()
 	{
-		this.price[ticket] = price;
+		return this.priceList;
 	}
+	public EnumMap<MovieType, Double> getMappedMovieTypePrice()
+	{
+		return this.mtList;
+	}
+	public Double[] getMtPriceList()
+	{
+		return this.mtPrice;
+	}
+
+
+	/*
+	 * setters
+	 */
+	public void setPrice(Double price, int index)
+	{
+		this.price[index] = price;
+	}
+	public void setMtPricePrice(Double resoprice, int index)
+	{
+		this.mtPrice[index] = resoprice;
+	}
+
 }
