e614c7610f3773cf2e7dac5fa8b82cc98d8b8308
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-02 23:58:19 +0800

    Changed attributes
    removed user, change movie to movie title (string)

--- MOBLIMA/src/entities/Booking.java
+++ MOBLIMA/src/entities/Booking.java
@@ -1,45 +1,47 @@
 package entities;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 
-public class Booking {
+public class Booking implements Serializable{
 	private String bookingID;
 	private double totalPrice;
-	private Movie movie;
+	private String movieTitle;
 	private Cinema cinema;
 	private ArrayList<Ticket> tt;
-	private User user;
+	//private User user;
 	private Showtime showtime;
 	
-	public Booking(String bookingID, double totalPrice, Movie movie, Cinema cinema, ArrayList<Ticket> tt, User user, Showtime showtime)
+	public Booking(String bookingID, double totalPrice, String movie, Cinema cinema, ArrayList<Ticket> tt, Showtime showtime, Transaction transaction)
 	{
 		this.bookingID = bookingID;
 		this.totalPrice = totalPrice;
-		this.movie = movie;
+		this.movieTitle = movie;
 		this.cinema = cinema;
 		this.tt = tt;
-		this.user = user;
+		//this.user = user;
 		this.showtime = showtime;
+		this.transaction = transaction;
 	}
 
 	
 	
 	public String getbookingID() {return this.bookingID;}
 	public double getTotalPrice() {return this.totalPrice;}
-	public Movie getMovie() {return this.movie;}
+	public String getMovie() {return this.movieTitle;}
 	public Cinema getCinema() {return this.cinema;}
 	public ArrayList<Ticket> getTicketList() {return this.tt;}
 	public Ticket getTicket(int index) {return this.tt.get(index);}
-	public User getUser() {return this.user;}
+	//public User getUser() {return this.user;}
 	public Showtime getShowtime() {return this.showtime;}
 	
 	public void setbookingID(String bookingID){this.bookingID = bookingID;}
 	public void setTotalPrice(double totalPrice){this.totalPrice = totalPrice;}
-	public void setMovie(Movie movie){this.movie = movie;}
+	public void setMovie(String movie){this.movieTitle = movie;}
 	public void setCinema(Cinema cinema){this.cinema = cinema;}
 	public void setTicketList(ArrayList<Ticket> tt){this.tt = tt;}
 	public void setTicket(int index, Ticket ticket) {this.tt.set(index, ticket);}
-	public void setUser(User user){this.user = user;}
+	//public void setUser(User user){this.user = user;}
 	public void setShowTime(Showtime showtime){this.showtime = showtime;}
 	
 	public void addTicket(Ticket tt)
