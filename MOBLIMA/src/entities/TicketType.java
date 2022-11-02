8ca0511138811b1def406192c493b63f5a6c2923
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-03 00:00:16 +0800

    Create TicketType.java

---
+++ MOBLIMA/src/entities/TicketType.java
@@ -0,0 +1,34 @@
+package entities;
+
+public enum TicketType {
+		MTW(0),
+		THURS(1),
+		FRIBS(2),
+		FRIAS(3),
+		WEEKEND(4),
+		SENIORWD(5),
+		STUDENTWD(6);
+		
+		private static final int size;
+		private int type;
+		
+		static
+		{
+			size = values().length;
+		}
+		
+		TicketType(int type)
+		{
+			this.type = type;
+		}
+		
+		public static int getSize()
+		{
+			return size;
+		}
+
+		public int getTT()
+		{
+			return this.type;
+		}
+}
