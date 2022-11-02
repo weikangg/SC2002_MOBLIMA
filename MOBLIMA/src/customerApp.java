59984f47370fb3b7da4479bbc6b9155c9ea5df9f
LingHin <66077386+LingHin@users.noreply.github.com>
2022-11-02 23:34:58 +0800

    Implement search and printing

--- MOBLIMA/src/customerApp.java
+++ MOBLIMA/src/customerApp.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.List;
 public class customerApp {
     private static customerApp newInstance = null;
     public static customerApp getInstance(){
@@ -13,17 +14,20 @@ public class customerApp {
 
     public void customerMenu(){
         
+        List<Movie> movie = new MovieListManager().getMovieList();
         boolean exit = false;
+        String strinput;
         int input;
         int counter = 0;
 
         do{
             //main menu
             System.out.print("================= MOBLIMA CUSTOMER INTERFACE =================\n"+
-            "1. Log In"+
-            "2. Create account"+
-            "2. Search\n"+
-            "3. Top 5 Movies\n"+
+            "1. Log In\n"+
+            "2. Create account\n"+
+            "3. Show all movies\n"+
+            "4. Search\n"+
+            "5. Top 5 Movies\n"+
             "0. Back to main menu\n"+
             "==============================================================\n"+
             "Please Enter Your Choice:");
@@ -44,10 +48,17 @@ public class customerApp {
                 //ask user for information to create account
 
                 case 3:
-                counter = 0;
-                //search for movie
-                
+                CustomerMovieManager.printMovieList(movie);
+                break;
+
                 case 4:
+                //search
+                System.out.println("Please enter the movie name:");
+                strinput = scan.nextLine();
+                CustomerMovieManager.searchMovie(movie, strinput, true);
+                break;
+
+                case 5:
                 counter = 0;
                 //show top 5 movies
                 
@@ -91,8 +102,27 @@ public class customerApp {
             switch(input){
                 case 1:
                 //book ticket
-                this.booking();
-                //after showing user the seats for a movie, call bookingmenu in booking manager
+                int cineplex;
+                String name;
+                List<Movie> movie;
+                movie = new MovieListManager().getMovieList();
+
+                //ask user for cineplex
+                System.out.println("Please choose a Cineplex:");
+                cineplex = scan.nextInt();
+                
+                //ask user for movie
+                System.out.println("Please choose a movie:");
+                CustomerMovieManager.printMovieList(movie);
+                name = scan.nextLine();
+
+                //link the movie to showtime
+
+                //show user movie timing
+
+                //ask user to choose a movie timing
+
+                //call bookingmenu in booking manager
 
                 case 2:
                 //show booking history
@@ -112,53 +142,5 @@ public class customerApp {
                 System.out.println("Please enter a valid option");
             }
         }while(exit == false);
-    }
-    
-    public void booking(){
-        int cineplex;
-        int count = 0;
-        List<Movie> movie;
-
-        
-
-        System.out.println("Please choose a Cineplex:");
-        cineplex = scan.nextInt();
-        
-        movie = new MovieListManager().getMovieList();
-        System.out.println("Please choose a movie:");
-        for(Movie m : movie) {
-            String cast, genre;
-            String casttmp = m.getCast();
-            String genretmp = m.getGenres();
-            String[] rating;
-
-            System.out.printf("----------------- MOVIE %d -----------------\n", count);
-            System.out.println("Movie Title: "+ m.getMovieTitle());
-            /*System.out.println("Showing Status: "+ m.getShowingStatus());
-            System.out.println("Synopsis: " + m.getSynopsis());
-            System.out.println("Movie Director: "+ m.getMovieDirector());
-            cast = casttmp.replaceAll(SplitBy, cvsSplitBy);
-            System.out.println("Casts: "+ cast);
-            cast = casttmp.replaceAll(SplitBy, cvsSplitBy);
-            genre = genretmp.replaceAll(SplitBy, cvsSplitBy);
-            System.out.println("Genres: "+ genre);
-            System.out.println("Movie Rating: " + m.getMovieRating());
-            System.out.println("Movie Duration: " + m.getMovieDuration());
-            double profitEarned = m.getProfitEarned();
-            BigDecimal bd = new BigDecimal(profitEarned);
-            System.out.println("Profit Earned: " + bd.toPlainString());
-            System.out.println("Overall Rating Score: " + m.getOverallRatingScore());
-            System.out.println("Release Date: " + m.getReleaseDate().toString());
-            System.out.println("Movie Type: " + m.getMovieType());
-            count++;*/
-            System.out.println("");
-        }
-        
-        //link the movie to showtime
-
-        //show user movie timing
-
-        //ask user to choose a movie timing
-        //call bookingmanager
     }
 }
