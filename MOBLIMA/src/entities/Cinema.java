3e58eb31924404cce743e15406421985faff7a14
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-05 22:51:28 +0800

    Update Cinema.java

--- MOBLIMA/src/entities/Cinema.java
+++ MOBLIMA/src/entities/Cinema.java
@@ -220,5 +220,9 @@ public class Cinema extends Cineplex{
         return cinemaClass.toString();
     }
     
+    public CinemaClass getCinemaClassCC(){
+        return this.cinemaClass;
+    }
+    
     
 }
