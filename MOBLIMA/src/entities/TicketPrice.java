ed19c08741233728237806b47569bfdf2eb47454
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-05 21:08:14 +0800

    Update TicketPrice.java

--- MOBLIMA/src/entities/TicketPrice.java
+++ MOBLIMA/src/entities/TicketPrice.java
@@ -1,13 +1,21 @@
 package entities;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.EnumMap;
 
 public class TicketPrice implements Serializable{
 	private EnumMap<TicketType, Double> priceList = new EnumMap<>(TicketType.class);
 	private EnumMap<MovieType, Double> mtList = new EnumMap<>(MovieType.class);
-	private Double[] price;
-	private Double[] mtPrice;
+	private ArrayList<Double> price;
+	private ArrayList<Double> mtPrice;
 	
 
 
@@ -16,20 +24,60 @@ public class TicketPrice implements Serializable{
 	 */
 	public TicketPrice()
 	{
-		this.price = new Double[]{8.50,9.50,9.50,11.0,11.0,4.0,7.0,12.0};
-		this.mtPrice = new Double[]{1.00,1.29,1.45,1.50};
+		String path = System.getProperty("user.dir") +"\\data\\ticketprice.csv";
+		String path2 = System.getProperty("user.dir") +"\\data\\movietypeprice.csv";
+		BufferedReader bufReader;
+		BufferedReader bufReaderTwo;
+        ArrayList<Double> price = new ArrayList<>();
+        ArrayList<Double> mtPrice = new ArrayList<>();
+        try {
+            bufReader = new BufferedReader(new FileReader(path));
+            
+            //listOfLines.add(0.2);
+            String line = bufReader.readLine();
+            while (line != null) 
+            {
+
+                price.add(Double.valueOf(line));
+				//System.out.println(line);
+                line = bufReader.readLine();
+            }
+            bufReader.close();
+        }
+        catch (FileNotFoundException e){}
+        catch (IOException e){}
+        try {
+            bufReaderTwo = new BufferedReader(new FileReader(path2));
+            
+            //listOfLines.add(0.2);
+            String line = bufReaderTwo.readLine();
+            while (line != null) 
+            {
+
+                mtPrice.add(Double.valueOf(line));
+				//System.out.println(line);
+                line = bufReaderTwo.readLine();
+            }
+            bufReaderTwo.close();
+        }
+        catch (FileNotFoundException e){System.out.println("TicketPrice error");}
+        catch (IOException e){System.out.println("TicketPrice error");}
+		//this.price = new Double[]{8.50,9.50,9.50,11.0,11.0,4.0,7.0,12.0};
+		//this.mtPrice = new Double[]{1.00,1.29,1.45,1.50};
 		int x = 0;
 		for(TicketType type: TicketType.values())
 		{
-			priceList.put(type,price[x]);
+			priceList.put(type,price.get(x));
 			x++;
 		}
 		x=0;
 		for(MovieType mt: MovieType.values())
 		{
-			mtList.put(mt,mtPrice[x]);
+			mtList.put(mt,mtPrice.get(x));
 			x++;
 		}
+		//System.out.println("Price 1:" + price.get(0));
+		//System.out.println("MTPrice 1:" + mtPrice.get(0));
 	}
 
 
@@ -38,13 +86,13 @@ public class TicketPrice implements Serializable{
 	 */
 	public Double getPrice(int index)
 	{
-		return this.price[index];
+		return this.price.get(index);
 	}
 	public Double getMtPrice(int index)
 	{
-		return this.mtPrice[index];
+		return this.mtPrice.get(index);
 	}
-	public Double[] getPrices()
+	public ArrayList<Double> getPrices()
 	{
 		return this.price;
 	}
@@ -56,7 +104,7 @@ public class TicketPrice implements Serializable{
 	{
 		return this.mtList;
 	}
-	public Double[] getMtPriceList()
+	public ArrayList<Double> getMtPriceList()
 	{
 		return this.mtPrice;
 	}
@@ -65,13 +113,55 @@ public class TicketPrice implements Serializable{
 	/*
 	 * setters
 	 */
-	public void setPrice(Double price, int index)
+	public void setPrice(int index, Double price)
 	{
-		this.price[index] = price;
+		this.price.set(index,price);
+		updatePrice();
 	}
-	public void setMtPricePrice(Double resoprice, int index)
+
+	public void updatePrice()
 	{
-		this.mtPrice[index] = resoprice;
+		String path = System.getProperty("user.dir") +"\\data\\ticketprice.csv";
+		//String path2 = System.getProperty("user.dir") +"\\data\\movietypeprice.csv";
+		ArrayList<String> arrList = new ArrayList<String>();
+		for(int i = 0; i<getPrices().size();i++)
+		{
+			arrList.add(getPrice(i).toString());
+		}
+    
+        Path output = Paths.get(path);
+        try {
+            Files.write(output, arrList);
+            System.out.println(output.toFile().getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
+	public void setMtPrice(int index, Double resoprice)
+	{
+		this.mtPrice.set(index,resoprice);
+	}
+
+	public void updateMTPrice()
+	{
+		//String path = System.getProperty("user.dir") +"\\data\\ticketprice.csv";
+		String path2 = System.getProperty("user.dir") +"\\data\\movietypeprice.csv";
+		ArrayList<String> arrList = new ArrayList<String>();
+		for(int i = 0; i<getMtPriceList().size();i++)
+		{
+			arrList.add(getMtPrice(i).toString());
+		}
+    
+        Path output = Paths.get(path2);
+        try {
+            Files.write(output, arrList);
+            System.out.println(output.toFile().getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+	}
+
+	
 
 }
