a2efd48d680896e1e3c3272aca5e41496dac8d0c
ktnnm <99934533+ktnnm@users.noreply.github.com>
2022-11-06 16:39:47 +0800

    Cineplex comments

--- MOBLIMA/src/entities/Cineplex.java
+++ MOBLIMA/src/entities/Cineplex.java
@@ -1,14 +1,47 @@
 package entities;
 
 import java.util.ArrayList;
-
+/**
+ * Represents a Cineplex managed by the vendor
+ * Has multiple Cinemas
+ * @author Andrew Leung
+ * @version 1.0
+ * @since 2022-11-06
+ */
 public class Cineplex {
+
+    /**
+     * Name of Cineplex
+     */
     private String name;
-    private int cineplexID;
+
+    /**
+     * Location of Cineplex
+     */
     private String location;
-    private int numCinemas; 
+
+     /**
+     * Number of Cinemas in Cineplex
+     */
+    private int numCinemas;
+
+    /**
+     * ID of Cineplex
+     */
+    private int cineplexID;
+    
+    /**
+     * Object array of Cinema objects
+     */
     private Cinema[] cinemas;
 
+    /**
+     * Creates a new Cineplex with the Cineplex's name, location, the number of cinemas and ID
+     * @param name Name of Cineplex
+     * @param location Location of Cineplex
+     * @param numCinemas Location of Cineplex
+     * @param cineplexID ID of Cineplex
+     */
     public Cineplex(String name, String location, int numCinemas, int cineplexID){
         this.name = name;
         this.location = location;
@@ -17,7 +50,8 @@ public class Cineplex {
     }
 
     /**
-     * Function to configure the list of cinemas
+     * Function to configure the array of Cinema objects cinemas[]
+     * @param str An ArrayList storing the classes of the Cinemas, indexed by cinemaID
      */
     public void configCinema(ArrayList<String> str){ 
 
@@ -45,6 +79,11 @@ public class Cineplex {
 
     }
 
+    /**
+     * Returns ArrayList of Showtimes in Cineplex that have movieID as the Movie's ID
+     * @param movieID ID that corresponds to a Movie
+     * @return list A list of Showtimes 
+     */
     public ArrayList<Showtime> searchMovie(int movieID){
         
         ArrayList<Showtime> list = new ArrayList<Showtime>(); 
@@ -59,22 +98,42 @@ public class Cineplex {
         return list;
     }
 
+    /**
+     * Function to return the Name of the Cineplex
+     * @return Name of Cineplex
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Function to return the ID of the Cineplex
+     * @return ID of Cineplex
+     */
     public int getCineplexID(){
         return cineplexID;
     }
 
+    /**
+     * Function to return the Location of the Cineplex
+     * @return Location of Cineplex
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Function to return the number of Cinemas in the Cineplex
+     * @return Number of Cinemas in Cineplex
+     */
     public int getNumCinema() {
         return numCinemas;
     }
 
+    /**
+     * Function to return the array of Cinema objects cinemas[]
+     * @return cinemas A list of Cinema objects
+     */
     public Cinema[] getCinemas() {
         return cinemas;
     }
