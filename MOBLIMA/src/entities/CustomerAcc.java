b935fed4fe28a791214bc3ae8ac693f8c9e29825
LingHin <laulinghin@gmail.com>
2022-11-07 16:14:48 +0800

    Update CustomerAcc.java

--- MOBLIMA/src/entities/CustomerAcc.java
+++ MOBLIMA/src/entities/CustomerAcc.java
@@ -1,13 +1,25 @@
 package entities;
+/**
+ * A class defining an Customer Account object. Inherits properties from the abstract Account class
+ * @author Ling Hin
+ * @version 2.0
+ * @since 07-11-2022
+ */
 
 import java.util.LinkedList;
 import java.util.ArrayList;
 public class CustomerAcc extends Account {
 
-    //create transaction history using linked list
-    private LinkedList<Transaction> bookingHistory = new LinkedList<Transaction>();
-
     //constructor
+    /**
+	 * This constructor is used to create a new Customer Account object
+	 * @param username	  This is the customer's username
+	 * @param email		  This is the customer's email
+	 * @param mobile	  This is the customer's mobile
+	 * @param age	      This is the customer's age
+	 * @param password	  This is the customer's password
+	 * @param accessLevel This is the customer's accessLevel
+	 */
     public CustomerAcc(String name, String email, int mobile, int age, String password, String accessLevel)
     {
         this.username = name;
@@ -17,58 +29,4 @@ public class CustomerAcc extends Account {
         this.password = password;
         this.accessLevel = accessLevel;
     }
-
-/*    public ArrayList<CustomerAcc> readCustomerFile(){
-        //read in account.csv file, then put the names, email, mobile and age into constructor and create an array of customer information
-
-    }
-
-    public CustomerAcc createAcc(){
-        String name;
-        String email;
-        int mobile;
-        int age;
-
-        //ask for user information and use try to check if name or email or mobile exists
-        
-        
-        
-        //to add exceptions
-        System.out.println("Enter username: ");
-        name = scan.nextLine();
-         
-        System.out.println("Enter your email:");
-        email = scan.nextLine();
-
-        System.out.println("Enter mobile:");
-        mobile = scan.nextInt();
-
-        System.out.println("Enter age:");
-        age = scan.nextInt();
-
-        CustomerAcc acc = new CustomerAcc(name, email, mobile, age);
-
-        //add into csv file
-        //for each account created, create a file inside tranaction
-    }
-*/
-    /*User's history will be update as the user uses the app. History will not be saved into csv file. If app shuts down, history will be gone
-    public void updateHistory(ArrayList<Ticket> tix, String movieName, String tranDateTime, String cinID){
-        //creates transaction id
-        String id =  cinID + tranDateTime;
-        //create transaction history
-        Transaction x = new Transaction(id, movieName, tranDateTime, tix);
-        //add the new transaction to history
-        bookingHistory.addFirst(x);
-        System.out.println("Transaction Complete!");
-    }*/
-
-    /*public void showBookingHistory(){
-        //prints out booking history
-        for(int i = 0; i < bookingHistory.size(); i++)
-        {
-        	bookingHistory.get(i).printTransaction();
-        }
-        return;
-    }*/
 }
