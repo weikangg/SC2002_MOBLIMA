c3d50fdd15c46cb000125a054f925304d0f16062
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-08 11:41:33 +0800

    Update TicketType.java

--- MOBLIMA/src/entities/TicketType.java
+++ MOBLIMA/src/entities/TicketType.java
@@ -1,16 +1,47 @@
 package entities;
-
+/**
+ * An enum class defining Ticket Type
+ * @author Jovan Sie
+ * @version 1.0
+ * @since 06-11-2022
+ */
 public enum TicketType {
+		/**
+		 * Ticket Type for Monday-Tuesday-Wednesday
+		 */
 		MTW(0),
+		/**
+		 * Ticket Type for Thursday
+		 */
 		THURS(1),
+		/**
+		 * Ticket Type for Friday Before 6pm
+		 */
 		FRIBS(2),
+		/**
+		 * Ticket Type for Friday After 6pm
+		 */
 		FRIAS(3),
+		/**
+		 * Ticket Type for Weekend
+		 */
 		WEEKEND(4),
+		/**
+		 * Ticket Type for Senior (Weekdays only)
+		 */
 		SENIORWD(5),
+		/**
+		 * Ticket Type for Student (Weekdays only)
+		 */
 		STUDENTWD(6),
+		/**
+		 * Ticket Type for Holidays
+		 */
 		HOLIDAY(7);
+		/**
+		 * index number
+		 */
 		private int type;
-
 		TicketType(int type)
 		{
 			this.type = type;
