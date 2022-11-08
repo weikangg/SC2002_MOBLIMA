3510af5f466c0e68ca0bcb09fb3fe244dedd8ce1
Aloysius Ng <aloysius.ng2000@hotmail.com>
2022-11-08 13:49:30 +0800

    Javadoc for pw checker

--- MOBLIMA/src/utils/PasswordStrengthChecker.java
+++ MOBLIMA/src/utils/PasswordStrengthChecker.java
@@ -1,7 +1,17 @@
 package utils;
-
+/**
+ * Util class that checks the strength of password entered during account creation
+ * Ensures password strength is strong before creating the account
+ * @author Aloysius
+ * @version 3.0
+ * @since 08-11-2022
+ */
 public class PasswordStrengthChecker {
-    
+    /**
+     * Function to check the strength of password entered
+     * @param pw password that needs to be evaluated
+     * @return "Strong", "Medium" or "Weak" depending on criteria met for passwords
+     */
     public static String passwordStrength(String pw){
         int pwlen = pw.length();
         boolean hasLower=false, hasUpper=false, hasDigit=false,hasSpecChar=false;
