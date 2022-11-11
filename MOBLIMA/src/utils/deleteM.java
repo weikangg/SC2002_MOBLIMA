931b81aa2596188aef45966812382d6a4683a22f
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-11 11:00:09 +0800

    Add files via upload

---
+++ MOBLIMA/src/utils/deleteM.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Interface to reset self, abstract methods resetSelf.
+ */
+public interface deleteM {
+	
+	// Methods that must be abstracted
+	
+	// A function must be implemented to reset the class
+	public void deleteM();
+	
+}
