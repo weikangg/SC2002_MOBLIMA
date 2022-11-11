0878528fe8c1c55a13353d014eea69c2917c85ea
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-11 16:10:40 +0800

    Update Transaction.java

--- MOBLIMA/src/entities/Transaction.java
+++ MOBLIMA/src/entities/Transaction.java
@@ -10,16 +10,28 @@ import java.util.ArrayList;
  * @since 06-11-2022
  */
 public class Transaction implements Serializable{
+    /**
+     * id of the transaction XXXYYYYMMDDhhm
+     */
     private String id;
+    /**
+     * string of the date time of the transaction
+     */
     private String tranDateTime;
+    /**
+     * ticket array list
+     */
     private ArrayList<Ticket> tix;
+    /**
+     * total price of the tickets in the ticket array
+     */
     private double totalPrice = 0;
 
     /**
      * Constructor
-     * @param id
-     * @param tranDateTime
-     * @param tix
+     * @param id current id of the transaction
+     * @param tranDateTime current date time of the transaction in string
+     * @param tix current ticket array list
      */
     public Transaction(String id, String tranDateTime, ArrayList<Ticket> tix)
     {
@@ -47,7 +59,7 @@ public class Transaction implements Serializable{
     
     /** 
      * gets the list of tickets purchased
-     * @return ArrayList<Ticket>
+     * @return current list of tickets purchased
      */
     public ArrayList<Ticket> getTicketList(){
         return this.tix;
@@ -56,7 +68,7 @@ public class Transaction implements Serializable{
     
     /** 
      * gets one of the tickets in the transaction
-     * @param index
+     * @param index index of the specific ticket
      * @return Ticket
      */
     public Ticket getTicket(int index){
@@ -78,32 +90,35 @@ public class Transaction implements Serializable{
      */
     /**
      * Set the transaction id
-     * @param id
+     * @param id new transaction id XXXYYYYMMDDhhm
      */
     public void setID(String id) {this.id = id;}
 
     /**
      * sets the date and time of the transaction
-     * @param tdt
+     * @param tdt new time and date of transaction in string
      */
     public void setTranDateTime(String tdt) {this.tranDateTime=tdt;}
     /**
      * sets the list of tickets of the transaction
-     * @param tix
+     * @param tix new list of tickets
      */
     public void setTicketList(ArrayList<Ticket> tix) {this.tix = tix;}
     /**
      * sets the ticket of the transaction
-     * @param index
-     * @param ticket
+     * @param index index of the specific ticket
+     * @param ticket new ticket
      */
     public void setTicket(int index, Ticket ticket) {this.tix.set(index, ticket);}
     /**
      * sets the total price of the transaction
-     * @param price
+     * @param price new total price of the transaction's ticket array
      */
     public void setTotalPrice(double price){this.totalPrice = price;}
 
+    /**
+     * prints the transaction details
+     */
     public void printTransaction() {
     	System.out.println("ID: " +this.getID());
 		System.out.println("Date & Time: " + this.getTranDateTime());
