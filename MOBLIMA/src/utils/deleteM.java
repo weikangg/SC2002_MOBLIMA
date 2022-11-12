26b8fbe4e94667a4b1e4f55a7f858568d6008a39
jvnsjh <101390444+jvnsjh@users.noreply.github.com>
2022-11-12 12:44:38 +0800

    Update deleteM.java

--- MOBLIMA/src/utils/deleteM.java
+++ MOBLIMA/src/utils/deleteM.java
@@ -3,14 +3,15 @@ package utils;
  * An interface to reset self, abstract methods resetSelf.
  * @author Jovan
  * @version 1.0
- * @since 11-11-2022
+ * @since 01-11-2022
  */
 
 public interface deleteM {
 	
-	// Methods that must be abstracted
-	
-	// A function must be implemented to reset the class
+	/** 
+	 * A function that must be implemented to reset the class
+	 * Methods that must be abstracted
+	 */
 	public void deleteM();
 	
 }
